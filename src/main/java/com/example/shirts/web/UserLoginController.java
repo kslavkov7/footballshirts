4a3bed634315be97cd96a3a0e2901f75b298b079
kslavkov7 <kiko19141914@gmail.com>
2021-12-10 14:46:10 +0200

    Fixed login validation.

--- src/main/java/com/example/shirts/web/UserLoginController.java
+++ src/main/java/com/example/shirts/web/UserLoginController.java
@@ -1,5 +1,6 @@
 package com.example.shirts.web;
 
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,4 +15,16 @@ public class UserLoginController {
         return "login";
     }
 
+    @PostMapping("/users/login-error")
+    public String failedLogin(
+            @ModelAttribute(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_USERNAME_KEY)
+                    String userName,
+            RedirectAttributes attributes
+    ) {
+
+        attributes.addFlashAttribute("bad_credentials", true);
+        attributes.addFlashAttribute("username", userName);
+
+        return "redirect:/users/login";
+    }
 }
